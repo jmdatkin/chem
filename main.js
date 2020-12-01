97fb5e6fe2eb1abe3362681d78a63e733d1215fe
Julian Atkin <jmd.atkin@gmail.com>
2020-12-01 19:58:28 +0000

    removed files irrelevant to repo

--- main.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    const canvasEl = document.querySelector(".canvas-wrapper");
-    canvasEl.setAttribute("width",window.innerWidth);
-    canvasEl.setAttribute("height",window.innerHeight);
-    Chem(canvasEl);
-};
- 
